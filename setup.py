cd47a0967ea32a9cc6653c36d745bb4621bbf028
fransua <serra.francois@gmail.com>
2019-10-16 12:55:01 +0200

    renamed scripts, added to setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='Meta-Waffle',
     version='0.0.1',
     packages=find_packages(),
-    scripts=['scripts/peaks_x_interaction.py'],
+    scripts=['scripts/waffle-peaks.py',
+             'scripts/waffle-bam2count.py'],
     license='GPLv3'
 )
