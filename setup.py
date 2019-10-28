75491cc3abb57d249be885c2b632d055038f689c
fransua <serra.francois@gmail.com>
2019-10-28 18:24:41 +0100

    simplified waffle-peaks

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='0.0.1',
     packages=find_packages(),
     scripts=['scripts/waffle-peaks.py',
+             'scripts/waffle-plot.py',
              'scripts/waffle-bam2count.py'],
     license='GPLv3'
 )
